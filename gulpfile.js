520146e266633e5822532be2979ae5ce59a8446f
Moritz Jacobs <mail@moritzjacobs.de>
2017-03-09 13:08:10 +0100

    Init

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp'),
+	gulpLoadPlugins = require('gulp-load-plugins'),
+	runSequence = require('run-sequence'),
+	del = require('del'),
+	plugins = gulpLoadPlugins();
+
+
+// Gulp plumber error handler
+var onErrorCSS = function (err) {
+		plugins.notify.onError({
+			subtitle: "Error in CSS File",
+			message: "<%= error.message %>"
+		})(err);
+		this.emit('end');
+};
+
+// Styles tasks
+gulp.task('css', function () {
+	return gulp.src('*.scss')
+		.pipe(plugins.plumber({
+			errorHandler: onErrorCSS
+		}))
+		.pipe(plugins.sourcemaps.init())
+		.pipe(plugins.sass({
+			outputStyle: 'expanded',
+			indentWidth: 4
+		}))
+		.pipe(plugins.autoprefixer('last 2 version'))
+		.pipe(plugins.sourcemaps.write('.'))
+		.pipe(gulp.dest('./'))
+		.pipe(plugins.notify({
+			message: 'Styles compiled'
+		}));
+
+});
+
+gulp.task('css:clean', function (cb) {
+	del(['*.map'], {
+		force: true
+	}, cb);
+});
+
+gulp.task('css:dist', function () {
+	runSequence('css', ['css:clean'])
+});
+
+
+// watch tasks
+gulp.task('watch', function () {
+	gulp.watch('*', ['css']);
+});
+
+// default tasks
+gulp.task('default', ['watch', 'css']);
+gulp.task('dist', ['css:dist']);
